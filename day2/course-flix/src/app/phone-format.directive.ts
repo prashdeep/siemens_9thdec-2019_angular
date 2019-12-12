180d9a106e8c57e9c40655d86fbca10a6f3ebafc
prashdeep <pradeep.kumar44@gmail.com>
2019-12-12 12:05:32 +0530

    committing the changes for custom directives

--- day2/course-flix/src/app/phone-format.directive.ts
+++ day2/course-flix/src/app/phone-format.directive.ts
@@ -1,10 +1,31 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[phoneFormat]'
 })
 export class PhoneFormatDirective {
 
-  constructor(private element: ElementRef) { }
+  @Input('phoneFormat') format;
+  
+
+  constructor(private element: ElementRef) {
+    console.log ('Inside the Phone format directive')
+   }
+   
+   ngOnInit(): void {
+     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+     //Add 'implements OnInit' to the class.
+     console.log('test ..'+this.format)
+   }
+
+   @HostListener("blur")
+   formatPhoneNumber(){
+     console.log('The value of format is '+ this.format);
+    let phoneNumberEntered:string = this.element.nativeElement.value;
+     if (!phoneNumberEntered.startsWith("+91")){
+      this.element.nativeElement.value = "+ 91 - "+ phoneNumberEntered;
+    }
+    
+   }
 
 }
