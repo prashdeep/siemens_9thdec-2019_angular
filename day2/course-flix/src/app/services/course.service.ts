f6b0267a2aa3d80aa9b2e9e968ae93935906fc02
prashdeep <pradeep.kumar44@gmail.com>
2019-12-13 08:34:12 +0530

    commmitting the environment configuration

--- day2/course-flix/src/app/services/course.service.ts
+++ day2/course-flix/src/app/services/course.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { Course } from '../model/Course';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  API_URL = "http://my-json-server.typicode.com/prashdeep/courseflix/courses";
-
+  
   getCourseDetails(courseId: number): Observable<Course> {
-    return this.httpClient.get<Course>(this.API_URL+"/"+courseId);
+    return this.httpClient.get<Course>(environment.API_URL+"/"+courseId);
   }
   
   
--- day2/course-flix/src/environments/environment.prod.ts
+++ day2/course-flix/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL : "http://my-json-server.typicode.com/prashdeep/courseflix/courses";
+
 };
